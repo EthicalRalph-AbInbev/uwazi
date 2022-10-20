a5058bcb24c17ed9adef4d5a76db7cd85dd72559
EthicalRalph <akinola.raphael@ng.ab-inbev.com>
2022-10-20 08:01:09 +0100

    chore: added swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,45 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { HttpExceptionFilter } from './filter/http-exception.filter';
+import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const logger = new Logger('UWAZI');
+const logger = new Logger('UWAZI');
 
-  app.enableCors();
-  app.setGlobalPrefix('/api');
+class Entrypoint {
+  public async start(): Promise<void> {
+    const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new HttpExceptionFilter());
+    this.setupSwagger(app);
 
-  const port = app.get(ConfigService).get('PORT');
+    app.enableCors();
+    app.setGlobalPrefix('/api');
 
-  await app.listen(port);
-  logger.log(`Application is running on: ${await app.getUrl()}`);
+    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalFilters(new HttpExceptionFilter());
+
+    const port = app.get(ConfigService).get('PORT');
+
+    await app.listen(port);
+    logger.log(`Application is running on: ${await app.getUrl()}`);
+  }
+
+  private setupSwagger(app: INestApplication): void {
+    const config = new DocumentBuilder()
+      .setTitle('UWAZI Orchestrator API')
+      .setDescription('The UWAZI Orchestrator API description')
+      .setVersion('0.1')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
+  }
 }
 
-bootstrap();
+new Entrypoint().start().catch((error) => {
+  logger.error(error, null, 'Bootstrap');
+
+  process.exit();
+});
--- src/product/dto/find-product.dto.ts
+++ src/product/dto/find-product.dto.ts
@@ -1,15 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumberString, IsString } from 'class-validator';
 
 export class FindProductDto {
   @IsNumberString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'Global trade item number of the item',
+  })
   globalTradeItemNumber: string;
 
   @IsNumberString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'Item serial number',
+  })
   serialNumber: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'Item destination country',
+  })
   destinationCountry: string;
 }
